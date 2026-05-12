89ee325198108936d3b6940b3ae3b67caf041b1e
dzejchym21 <karol3jachym@gmail.com>
2026-05-12 19:30:51 +0200

    added CpuData struct

---
+++ include/CpuData.h
@@ -0,0 +1,16 @@
+#ifndef CPP_SYSMON_CPUDATA_H
+#define CPP_SYSMON_CPUDATA_H
+
+struct CpuData {
+    long user, nice, system, idle, iowait, irq, softirq;
+
+    long getTotalTime() const {
+        return user + nice + system + idle + irq + softirq;
+    }
+
+    long getIdleTime() const {
+        return idle + iowait;
+    }
+};
+
+#endif //CPP_SYSMON_CPUDATA_H
